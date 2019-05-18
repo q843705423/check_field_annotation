3545975776ae71a0b720ad3c03e85d0a2a240e22
qiqi.chen <qiqi.chen@teradata.com>
2019-05-18 16:39:51 +0800

    null be no_error

--- src/util/FiledChecker.java
+++ src/util/FiledChecker.java
@@ -8,6 +8,9 @@ import annotation.RegularRule;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,106 +20,102 @@ public class FiledChecker {
     private final static String MAX_LENGTH_NOT_STRING_ERROR = "error annotation MaxLengthRule for %s , must be String";
     private final static String REGULAR_LENGTH_NOT_STRING_ERROR = "error annotation RegularRule for %s , must be String";
     private final static String methodNameTemplate = "get%s";
+    private final static String NO_ERROR = null;
 
-
-    /**
-     * if return null, it mean it is ok.
-     *
-     * @param object the object you want to check.
-     * @return check and get error info
-     */
     public String checkAndGetErrorInfo(Object object) {
         Class<?> clazz = object.getClass();
         Field[] fields = clazz.getDeclaredFields();
-        String error;
-        for (Field field : fields) {
-            error = checkNullable(field, object, clazz);
-            if (error != null) {
+        Optional<String> errorInfo = Arrays.stream(fields).map(field -> {
+
+            String error = checkNullable(field, object, clazz);
+            if (Objects.nonNull(error)) {
                 return error;
             }
             error = checkMinLength(field, object, clazz);
-            if (error != null) {
+            if (Objects.nonNull(error)) {
                 return error;
             }
             error = checkMaxLength(field, object, clazz);
-            if (error != null) {
+            if (Objects.nonNull(error)) {
                 return error;
             }
             error = checkRegular(field, object, clazz);
-            if (error != null) {
+            if (Objects.nonNull(error)) {
                 return error;
             }
-        }
-        return null;
+            return NO_ERROR;
+        }).filter(Objects::nonNull).findFirst();
+
+        return errorInfo.orElse(NO_ERROR);
     }
 
     private String checkRegular(Field field, Object object, Class<?> clazz) {
         RegularRule regularRule = field.getAnnotation(RegularRule.class);
-        if (regularRule == null) {
-            return null;
+        if (Objects.isNull(regularRule)) {
+            return NO_ERROR;
         }
         if (isNotStringField(field)) {
             return String.format(REGULAR_LENGTH_NOT_STRING_ERROR, field.getName());
         }
         Pattern compile = Pattern.compile(regularRule.expression());
         Object invokeValue = getValue(field, clazz, object);
-        if (invokeValue == null) {
-            return null;
+        if (Objects.isNull(invokeValue)) {
+            return NO_ERROR;
         }
         String invokeValueString = invokeValue.toString();
         Matcher matcher = compile.matcher(invokeValueString);
         if (!matcher.find()) {
             return regularRule.message();
         }
-        return null;
+        return NO_ERROR;
     }
 
     private String checkMinLength(Field field, Object object, Class<?> clazz) {
         MinLengthRule minLengthRule = field.getAnnotation(MinLengthRule.class);
-        if (minLengthRule == null) {
-            return null;
+        if (Objects.isNull(minLengthRule)) {
+            return NO_ERROR;
         }
         if (isNotStringField(field)) {
             return String.format(MIN_LENGTH_NOT_STRING_ERROR, field.getName());
         }
         Object invokeValue = getValue(field, clazz, object);
-        if (invokeValue == null) {
+        if (Objects.isNull(invokeValue)) {
             return minLengthRule.message();
         }
         if (invokeValue.toString().length() < minLengthRule.minLength()) {
             return minLengthRule.message();
         }
-        return null;
+        return NO_ERROR;
     }
 
 
     private String checkMaxLength(Field field, Object object, Class<?> clazz) {
         MaxLengthRule maxLengthRule = field.getAnnotation(MaxLengthRule.class);
-        if (maxLengthRule == null) {
-            return null;
+        if (Objects.isNull(maxLengthRule)) {
+            return NO_ERROR;
         }
         if (isNotStringField(field)) {
             return String.format(MAX_LENGTH_NOT_STRING_ERROR, field.getName());
         }
         Object invokeValue = getValue(field, clazz, object);
-        if (invokeValue != null) {
+        if (Objects.nonNull(invokeValue)) {
             String stringValue = invokeValue.toString();
             if (stringValue.length() > maxLengthRule.maxLength()) {
                 return maxLengthRule.message();
             }
         }
-        return null;
+        return NO_ERROR;
     }
 
     private String checkNullable(Field field, Object object, Class<?> clazz) {
         NotNullRule notNullRule = field.getAnnotation(NotNullRule.class);
-        if (notNullRule != null) {
+        if (Objects.nonNull(notNullRule)) {
             Object invokeValue = getValue(field, clazz, object);
-            if (invokeValue == null) {
+            if (Objects.isNull(invokeValue)) {
                 return notNullRule.message();
             }
         }
-        return null;
+        return NO_ERROR;
     }
 
     private Object getValue(Field field, Class<?> clazz, Object object) {
@@ -128,9 +127,9 @@ public class FiledChecker {
         } catch (NoSuchMethodException | IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
-            return null;
+            return NO_ERROR;
         }
-        return null;
+        return NO_ERROR;
     }
 
     private boolean isNotStringField(Field field) {
